2770e35b92dc5186eb5b75099af2a097d9370309
MechialH <mechial.harrison@gmail.com>
2021-05-06 21:20:25 -0400

    cors added

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cart = require("./cart");
+const cors = require("cors")
 const app = express();
 
 app.use("/", cart);
+app.use(cors())
 
 const port = 3000;
 app.use(express.json())
